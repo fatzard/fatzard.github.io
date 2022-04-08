d53f50167beb0becdd2498a96459b4962e16cea0
fatzard <fatzard_h21@outlook.com>
2022-04-08 23:26:16 +0800

    Site updated: 2022-04-08 23:26:12

--- js/matery.js
+++ js/matery.js
@@ -103,9 +103,12 @@ $(function () {
         $('#articleContent, #myGallery').lightGallery({
             selector: '.img-item',
             // 启用字幕
-            subHtmlSelectorRelative: true
+            subHtmlSelectorRelative: true,
+            showThumbByDefault: false   //这句加上
         });
-
+        $(document).find('img[data-original]').each(function(){
+            $(this).parent().attr("href", $(this).attr("data-original"));
+        });
         // progress bar init
         const progressElement = window.document.querySelector('.progress-bar');
         if (progressElement) {
